c131a05bb1291aa473a50676f3b81c7eebc0ed06
Andy <menaandy198@gmail.com>
2025-01-15 21:42:48 -0600

    Mejorando la logica de algunos controladores

--- server/src/controllers/barberController.ts
+++ server/src/controllers/barberController.ts
@@ -7,233 +7,221 @@ import { col, fn, literal, Op } from "sequelize";
 import AppointmentService from "../models/AppointmentService";
 
 declare global {
-    namespace Express {
-        interface Request {
-            file?: Express.Multer.File; // Aquí usamos el tipo `File` proporcionado por `multer`
-        }
+  namespace Express {
+    interface Request {
+      file?: Express.Multer.File; // Aquí usamos el tipo `File` proporcionado por `multer`
     }
+  }
 }
 
 
 class BarberController {
 
-    public static getAllBarber = async (request: Request, response: Response) => {
+  public static getAllBarber = async (request: Request, response: Response) => {
 
-        try {
-            const barber = await Barber.findAll();
-            response.status(200).json(barber);
+    try {
+      const barber = await Barber.findAll();
+      response.status(200).json(barber);
 
-        } catch (error) {
-            console.log(error)
-            const errorMessage = new Error('Oops! Something went wrong');
-            response.status(500).json({ error: errorMessage.message });
-        }
-
+    } catch (error) {
+      console.log(error)
+      const errorMessage = new Error('Oops! Something went wrong');
+      response.status(500).json({ error: errorMessage.message });
     }
 
-
-    public static getBarberById = async (request: Request, response: Response) => {
-
-        try {
-
-            const barberId = parseInt(request.params.barberId);
-            const barber = await Barber.findByPk(barberId);
-            if (!barber) {
-                const errorMessage = new Error('Barber not found');
-                return response.status(404).json({ error: errorMessage.message });
-            }
-
-            response.status(200).json(barber);
-
-        } catch (error) {
-            const errorMessage = new Error('Oops! Something went wrong');
-            response.status(500).json({ error: errorMessage.message });
-        }
-
-    }
+  }
 
 
-    public static addBarber = async (request: Request, response: Response) => {
+  public static getBarberById = async (request: Request, response: Response) => {
 
-        try {
+    try {
 
-            const { name, lastname, email, phone, specialty } = request.body;
-            const barber = await Barber.findOne(email);
-
-            if (!barber) {
-                const error = new Error('Este barbero ya existe');
-                return response.status(409).json({ error: error.message });
-            }
-
-            const newBarber = await Barber.create({
-                name,
-                lastname,
-                email,
-                phone,
-                specialty,
-                image: request.file ? request.file.filename : 'default.png'
-            });
-
-            await newBarber.save();
-
-            response.send('Barbero registrado correctamente')
-
-        } catch (error) {
-            console.log(error)
-            const errorMessage = new Error('Oops! Something went wrong');
-            response.status(500).json({ error: errorMessage.message });
-        }
-
-    }
-
-    public static updateBarber = async (request: Request, response: Response) => {
-      try {
-        const barberId = parseInt(request.params.barberId);
-        const { name, lastname, email, phone, specialty } = request.body;
-        const barber = await Barber.findByPk(barberId);
-
-        if (!barber) {
-          const errorMessage = new Error('Barber0 no encontrado');
-          return response.status(404).json({ error: errorMessage.message });
-        };
-
-        barber.name = name || barber.name;
-        barber.lastname = lastname || barber.lastname;
-        barber.email = email || barber.email;
-        barber.phone = phone || barber.phone;
-        barber.specialty = specialty || barber.specialty;
-        
-        const imageExist = barber.image;
-        
-        if(request.file){
-          barber.image = request.file.filename;
-          
-          if(imageExist && imageExist !== 'default.png'){
-            const previousImageUrl = path.join(__dirname, '..', 'uploads', imageExist);
-            fs.unlinkSync(previousImageUrl);
-          }
-        }
-
-        await barber.save();
-
-        response.status(200).json(barber);
-      } catch (error) {
-        console.log(error);
-        const errorMessage = new Error('Oops! Something went wrong');
-        response.status(500).json({ error: errorMessage.message });
+      const barberId = parseInt(request.params.barberId);
+      const barber = await Barber.findByPk(barberId);
+      if (!barber) {
+        const errorMessage = new Error('Barber not found');
+        return response.status(404).json({ error: errorMessage.message });
       }
+
+      response.status(200).json(barber);
+
+    } catch (error) {
+      const errorMessage = new Error('Oops! Something went wrong');
+      response.status(500).json({ error: errorMessage.message });
     }
 
-    public static deleteBarber = async (request: Request, response: Response) => {
-      try {
-        const barberId = parseInt(request.params.barberId);
-        const barber = await Barber.findByPk(barberId);
-        const image = barber.image;
+  }
 
-        if (!barber) {
-          const errorMessage = new Error('Barbero no encontrado');
-          return response.status(404).json({ error: errorMessage.message });
-        };
 
-        await barber.destroy();
-          
-        const previousImageUrl = path.join(__dirname, '..', 'uploads', image);
-        fs.unlinkSync(previousImageUrl);
+  public static addBarber = async (request: Request, response: Response) => {
 
-        response.status(200).send('Barbero eliminado correctamente');
-      } catch (error) {
-        console.log(error);
-        const errorMessage = new Error('Oops! Something went wrong');
-        response.status(500).json({ error: errorMessage.message });
+    try {
+
+      const { name, lastname, email, phone, specialty } = request.body;
+      const barber = await Barber.findOne(email);
+
+      if (!barber) {
+        const error = new Error('Este barbero ya existe');
+        return response.status(409).json({ error: error.message });
       }
+
+      const newBarber = await Barber.create({
+        name,
+        lastname,
+        email,
+        phone,
+        specialty,
+        image: request.file ? request.file.filename : 'default.png'
+      });
+
+      await newBarber.save();
+
+      response.status(201).json(newBarber)
+
+    } catch (error) {
+      console.log(error)
+      const errorMessage = new Error('Oops! Something went wrong');
+      response.status(500).json({ error: errorMessage.message });
     }
 
-    public static citasBarberos = async (request: Request, response: Response) => {
-      try {
-        const { mes, year } = request.query;
+  }
 
-        if (!mes || !year) {
-          return response.status(400).json({ message: "Mes y año son requeridos." });
-        };
+  public static updateBarber = async (request: Request, response: Response) => {
+    try {
+      const barberId = parseInt(request.params.barberId);
+      const { name, lastname, email, phone, specialty } = request.body;
+      const barber = await Barber.findByPk(barberId);
 
-        const startDate = new Date(Number(year), Number(mes) - 1, 1); // Primer día del mes
-        const endDate = new Date(Number(year), Number(mes), 0); // Último día del mes
+      if (!barber) {
+        const errorMessage = new Error('Barber0 no encontrado');
+        return response.status(404).json({ error: errorMessage.message });
+      };
 
-        const barberos = await Barber.findAll({
-          attributes: [
-            'barberId',
-            'name',
-            [
-              fn('COUNT', col('Appointment.appointmentId')), 'appointments'
-            ]
-          ],
-            include: [
-            {
-              model: Appointment,
-              attributes: [],
-              where: {
-                date: { [Op.between]: [startDate, endDate] }
-              },
-              required: false
-            }
-          ],
-          group: ['Barber.barberId'],
-          order: [[literal('appointments'), 'DESC']]
-        });
+      barber.name = name || barber.name;
+      barber.lastname = lastname || barber.lastname;
+      barber.email = email || barber.email;
+      barber.phone = phone || barber.phone;
+      barber.specialty = specialty || barber.specialty;
 
-        response.status(200).json(barberos);
-      } catch (error) {
-        console.log(error);
-        const errorMessage = new Error('Oops! Something went wrong');
-        response.status(500).json({ error: errorMessage.message });
+      const imageExist = barber.image;
+
+      if (request.file) {
+        barber.image = request.file.filename;
+
+        if (imageExist && imageExist !== 'default.png') {
+          const previousImageUrl = path.join(__dirname, '..', 'uploads', imageExist);
+          fs.unlinkSync(previousImageUrl);
+        }
       }
+
+      await barber.save();
+
+      response.status(200).json(barber);
+    } catch (error) {
+      console.log(error);
+      const errorMessage = new Error('Oops! Something went wrong');
+      response.status(500).json({ error: errorMessage.message });
     }
+  }
 
-    public static ingresosBarberos = async (request: Request, response: Response) => {
-      try {
-        const { mes, year } = request.query;
+  public static deleteBarber = async (request: Request, response: Response) => {
+    try {
+      const barberId = parseInt(request.params.barberId);
+      const barber = await Barber.findByPk(barberId);
+      const image = barber.image;
 
-        if (!mes || !year) {
-          return response.status(400).json({ message: "Mes y año son requeridos." });
-        };
+      if (!barber) {
+        const errorMessage = new Error('Barbero no encontrado');
+        return response.status(404).json({ error: errorMessage.message });
+      };
+      const imagePath = path.join(__dirname, '..', 'uploads', image);
 
-        const startDate = new Date(Number(year), Number(mes) - 1, 1); // Primer día del mes
-        const endDate = new Date(Number(year), Number(mes), 0); // Último día del mes
+      // Elimina al barbero de la base de datos
+      await barber.destroy();
 
-        const barberoIngresos = await AppointmentService.findAll({
-          attributes: [
-            [col('Appointment.barberId'), 'barberId'],
-            [col('Appointment.barbero.name'), 'name'],
-            [fn('SUM', col('current_price')), 'value'],
-          ],
-          include: [
-            {
-              model: Appointment,
-              attributes: [],
-              where: {
-                date: {
-                  [Op.between]: [startDate, endDate],
-                },
-              },
-              include: [
-                {
-                  model: Barber,
-                  attributes: [], 
-                },
-              ],
+      // Verifica si la imagen existe antes de intentar eliminarla
+      if (fs.existsSync(imagePath)) {
+        fs.unlinkSync(imagePath);
+      }
+
+      response.status(200).send('Barbero eliminado correctamente');
+
+
+    } catch (error) {
+      console.log(error);
+      const errorMessage = new Error('Oops! Something went wrong');
+      response.status(500).json({ error: errorMessage.message });
+    }
+  }
+
+  public static citasBarberos = async (request: Request, response: Response) => {
+    try {
+    
+      const barberos = await Barber.findAll({
+        attributes: [
+          'barberId',
+          'name',
+          [
+            fn('COUNT', col('Appointment.appointmentId')), 'appointments'
+          ]
+        ],
+        include: [
+          {
+            model: Appointment,
+            attributes: [],
+            where: {
+              status: 'completed'
             },
-          ],
-          group: ['Appointment.barberId'], 
-          order: [[literal('value'), 'DESC']], 
-        });
+            required: false
+          }
+        ],
+        group: ['Barber.barberId'],
+        order: [[literal('appointments'), 'DESC']]
+      });
 
-        response.status(200).json(barberoIngresos);
-      } catch (error) {
-        console.log(error);
-        const errorMessage = new Error('Oops! Something went wrong');
-        response.status(500).json({ error: errorMessage.message });
-      }
+      response.status(200).json(barberos);
+    } catch (error) {
+      console.log(error);
+      const errorMessage = new Error('Oops! Something went wrong');
+      response.status(500).json({ error: errorMessage.message });
     }
+  }
+
+  public static ingresosBarberos = async (request: Request, response: Response) => {
+    try {
+
+      const barberoIngresos = await AppointmentService.findAll({
+        attributes: [
+          [col('Appointment.barberId'), 'barberId'],
+          [col('Appointment.barbero.name'), 'name'],
+          [fn('SUM', col('current_price')), 'value'],
+        ],
+        include: [
+          {
+            model: Appointment,
+            attributes: [],
+            where: {
+              status: 'completed'
+            },
+            include: [
+              {
+                model: Barber,
+                attributes: [],
+              },
+            ],
+          },
+        ],
+        group: ['Appointment.barberId'],
+        order: [[literal('value'), 'DESC']],
+      });
+
+      response.status(200).json(barberoIngresos);
+    } catch (error) {
+      console.log(error);
+      const errorMessage = new Error('Oops! Something went wrong');
+      response.status(500).json({ error: errorMessage.message });
+    }
+  }
 }
 
 export default BarberController;
