8d7a8ff9d267f7b51701eeb1e56e4899bf54b29d
Andy <menaandy198@gmail.com>
2025-03-20 10:27:34 -0600

    refactor(authController): refactorizando el codigo authController, aplicando SOLID

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,287 +1,89 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import User from "../models/User";
-import { generateToken } from "../helpers/token";
 import Token from "../models/Token";
-import AuthEmail from "../emails/authEmail";
-import { comparePassword, hashPassword } from "../helpers/auth";
-import { generateJWT } from "../helpers/jwt";
-
-
+import { IAuthService } from "../interfaces/services/IAuthService";
 
 class AuthController {
 
-    public static createAccount = async (request: Request, response: Response) => {
+    constructor
+        (
+            private authService: IAuthService
+        ) { }
 
+
+    public createAccount = async (request: Request, response: Response, next: NextFunction) => {
         try {
-
-            const { email, password }: User = request.body;
-
-            const userExist = await User.findOne({ where: { email } });
-
-            if (userExist) {
-                const error = new Error('Este usuario ya existe');
-                response.status(404).json({ error: error.message });
-                return;
-            }
-
-            const token = new Token();
-            const user = new User(request.body);
-
-            //encriptar el password
-            user.password = await hashPassword(password)
-
-            await user.save();
-
-            token.token = generateToken();
-            token.userId = user.userId;
-
-            await AuthEmail.sendEmailConfirmAccount({
-                email: user.email,
-                token: token.token,
-                name: user.name,
-                lastname: user.lastname
-            })
-
-            await token.save();
-
+            await this.authService.createAccount(request.body);
             response.send('Hemos enviado instrucciones a tu correo para confirmar tu cuenta');
-
         } catch (error) {
-            response.status(400).json({ msg: error.message })
+            next(error)
         }
     }
 
-    public static confirmAccount = async (request: Request, response: Response) => {
-
+    public confirmAccount = async (request: Request, response: Response, next: NextFunction) => {
         try {
-
-            const { token } = request.body;
-
-            const tokenExist = await Token.findOne({ where: { token } });
-
-            if(!tokenExist){
-                const error = new Error('Token no válido o expirado');
-                return response.status(404).json({ msg: error.message });
-            }
-
-            const user = await User.findByPk(tokenExist.userId);
-
-            user.confirmed = 1;
-
-            await Promise.allSettled([
-                user.save(),
-                tokenExist.destroy()
-            ])
-
-            response.status(200).send('Cuenta confirmada correctamente, puedes iniciar sesión');
-
+            const token: Token['token'] = request.body.token;
+            await this.authService.confirmAccount(token);
+            response.send('Cuenta confirmada correctamente');
         } catch (error) {
-            response.status(400).json({ msg: error.message })
+            next(error)
         }
     }
 
-    public static login = async (request: Request, response: Response) => {
-
+    public login = async (request: Request, response: Response, next: NextFunction) => {
         try {
-            
-            const {email , password} = request.body;
-
-            const user = await User.findOne({ where: { email } });
-
-            if(!user){
-                const error = new Error('Usuario no encontrado');
-                return response.status(404).json({ error: error.message });
-            }
-
-            //si el usuario no ha confirmado su cuenta enviamos un email con el token en automático
-            if(!user.confirmed){
-
-                const token = new Token();
-                token.token = generateToken();
-                token.userId = user.userId;
-                await token.save();
-
-                await AuthEmail.sendEmailConfirmAccount({
-                    email: user.email,
-                    token: token.token,
-                    name: user.name,
-                    lastname: user.lastname
-                })
-
-                const error = new Error('Cuenta no confirmada, hemos enviado un email de confirmación a tu correo');
-
-                return response.status(401).json({ error: error.message });;
-            }
-
-            const isPasswordCorrect = await comparePassword(password, user.password);
-
-            if(!isPasswordCorrect){
-                const error = new Error('Contraseña incorrecta');
-                return response.status(401).json({ error: error.message });
-            }
-
-            //autenticar al usuario
-
-            const token = generateJWT({id: user.userId, admin: user.admin});
+            const { email, password } = request.body;
+            const token = await this.authService.login(email, password);
             response.status(200).send(token);
-
         } catch (error) {
-            console.log(error);
-            
+            next(error)
         }
-
-
-
     }
-    
-    public static requestConfirmationToken = async(request: Request, response: Response) => {
 
+    public requestConfirmationToken = async (request: Request, response: Response, next: NextFunction) => {
         try {
-
             const { email } = request.body;
-
-
-            const user = await User.findOne({ where: { email } });
-
-            if(!user){
-                const error = new Error('Usuario no registrado');
-                return response.status(404).json({ error: error.message });
-            }
-
-            if(user.confirmed){
-                const error = new Error('Este usuario ya ha confirmado su cuenta');
-                return response.status(403).json({ error: error.message });
-            }
-    
-
-            //Almacenar el token al nuevo usuario
-            //Crear la instancia del token para el usuario
-            const token = new Token();
-            token.token = generateToken();
-            token.userId = user.userId;
-
-            await user.save();
-
-            //enviar email
-            await AuthEmail.sendEmailConfirmAccount({
-                email: user.email,
-                token: token.token,
-                name: user.name,
-                lastname: user.lastname
-            });
-
-            await token.save();
-
+            await this.authService.requestConfirmationToken(email);
             response.status(200).send('Hemos enviado un email de confirmación a tu correo');
-            
         } catch (error) {
-            console.log(error)
+            next(error)
         }
-
     }
 
-    public static forgotPassword = async ( request: Request, response: Response) => {
-
+    public forgotPassword = async (request: Request, response: Response, next: NextFunction) => {
         try {
             const { email } = request.body;
-
-            console.log(email)
-
-            const user = await User.findOne({
-                where: {email}
-            })
-
-            if (!user) {
-                const error = new Error('Este usuario no esta registrado');
-                return response.status(404).json({ error: error.message });
-            }
-            
-            //Almacenar el token al nuevo usuario
-            //Crear la instancia del token para el usuario
-            const token = new Token();
-            token.token = generateToken();
-            token.userId = user.userId;
-
-            //enviar email de recuperación de password;
-
-            await AuthEmail.sendEmailResetPassword({
-                email: user.email,
-                token: token.token,
-                name: user.name,
-                lastname: user.lastname
-            });
-           
-
-            await token.save();
-
-            //enviar el correo de confirmación al usuario
-            response.send('Se ha enviado un nuevo token, revisa tu email');
-
-        } catch (err) {
-            console.log(err)
-            response.status(500).json({ error: 'Hubo un error' })
+            await this.authService.forgotPassword(email);
+            response.status(200).send('Hemos enviado un email para restablecer tu contraseña');
+        } catch (error) {
+            next(error)
         }
-
     }
 
 
-    public static validateToken = async (request: Request, response: Response) => {
-
+    public validateToken = async (request: Request, response: Response, next: NextFunction) => {
         try {
-
             //leemos el token del formulario
             const token: string = request.body.token;
-
-            //verificar si el token existe
-            const tokenExist = await Token.findOne({ where: { token } });
-
-            //si el token no existe, enviamos un error
-            if (!tokenExist) {
-                const error = new Error('Token no válido o expirado');
-                return response.status(404).json({ error: error.message });
-            }
-
-            response.status(200).send('Token Valido. Define tu nueva Contraseña');
-
+            await this.authService.validateToken(token);
+            response.status(200).send('Token válido, define tu nueva password');
         } catch (error) {
-            response.status(500).json({ error: 'Hubo un error' })
+            next(error)
         }
-
     }
 
-    public static updatePasswordWithToken = async ( request: Request, response: Response) => {
-
+    public updatePasswordWithToken = async (request: Request, response: Response, next: NextFunction) => {
         try {
-
-            const token = request.params.token;
-            const password = request.body.password;
-
-            const tokenExist = await Token.findOne({ where: { token } });
-
-            if(!tokenExist){
-                const error = new Error('Token no válido o expirado');
-                return response.status(404).json({ error: error.message });
-            }
-
-            const user = await User.findByPk(tokenExist.userId);
-
-            user.password = await hashPassword(password)
-
-            await Promise.all([
-                user.save(),
-                tokenExist.destroy()
-            ])
-
+            const { token } = request.params;
+            const { password } = request.body
+            await this.authService.updatePasswordWithToken(token, password)
             response.status(200).send('Contraseña actualizada correctamente');
-            
         } catch (error) {
-            response.status(500).json({ error: 'Hubo un error' })
-            
+            next(error)
         }
-
     }
 
-    public static getProfile = async ( request: Request, response: Response ) => {
+    public getProfile = async (request: Request, response: Response) => {
         try {
             const user = await User.findByPk(request.user.userId, {
                 attributes: ['name', 'lastname', 'image', 'userId', 'phone', 'address', 'admin', 'email']
@@ -290,7 +92,6 @@ class AuthController {
         } catch (error) {
             response.status(500).json({ error: 'Hubo un error' })
         }
-
     }
 }
 
